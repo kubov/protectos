8e94c51197b4ff755ca989e75f670a9be1ee30cb
kubov <kubov2@gmail.com>
2015-10-20 00:40:00 +0200

    String writing and friends

--- fb.c
+++ fb.c
@@ -1,8 +1,54 @@
 #include "fb.h"
+#include "io.h"
 
 uint16_t  *_fb = (uint16_t*)0xb8000;
+uint16_t _pos = 0;
 
-void fb_write(uint32_t i, char c, uint8_t fg, uint8_t bg)
+void fb_set_cell(uint32_t i, char c, uint8_t fg, uint8_t bg)
 {
-  _fb[i] = ((fg | bg) << 8) | c;
+    _fb[i] = ((fg | bg) << 8) | c;
+}
+
+void fb_clear()
+{
+    _pos = 0;
+
+    for (int i=0; i< FB_WIDTH*FB_HEIGHT; i++)
+        fb_set_cell(i, ' ', FB_BLACK, FB_BLACK);
+}
+
+void fb_move(uint16_t pos)
+{
+    out(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+    out(FB_DATA_PORT, ((pos >> 8) & 0xff));
+    out(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+    out(FB_DATA_PORT, pos & 0xff);
+}
+
+void fb_write_char(char c)
+{
+    uint8_t draw = 1;
+    if (c == '\n')
+    {
+        _pos += FB_WIDTH - (_pos % FB_WIDTH);
+        draw = 0;
+    }
+    else
+    {
+        fb_set_cell(_pos, c, FB_BLACK, FB_GREEN);
+        _pos += 1;
+    }
+
+    fb_move(_pos);
+}
+
+void fb_write(char* buf, int len)
+{
+    for (int i=0; i<len; i++)
+        fb_write_char(buf[i]);
+}
+
+void fb_write_str(char* buf)
+{
+    while(*buf) fb_write_char(*buf++);
 }
--- fb.h
+++ fb.h
@@ -5,5 +5,16 @@
 #define FB_WHITE 15
 #define FB_GREEN 10
 
-void fb_write(uint32_t i, char c, uint8_t fg, uint8_t bg);
+#define FB_COMMAND_PORT         0x3D4
+#define FB_DATA_PORT            0x3D5
+#define FB_HIGH_BYTE_COMMAND    14
+#define FB_LOW_BYTE_COMMAND     15
 
+#define FB_WIDTH 80
+#define FB_HEIGHT 25
+
+void fb_set_cell(uint32_t i, char c, uint8_t fg, uint8_t bg);
+void fb_write_char(char c);
+void fb_write(char* buf, int len);
+void fb_clear();
+void fb_write_str(char* buf);
